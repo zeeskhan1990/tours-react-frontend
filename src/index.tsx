306a9aaa14227bcb3a959b72db85057c3310f662
Zeeshan Khan <zeeskhan1990@gmail.com>
2019-12-04 00:12:52 +0530

    style ext

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+//import "./custom.bootstrap.css"
 import "./custom.css";
 
 ReactDOM.render(
